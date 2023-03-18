b4baee5e9db80b1c71b898dada9cab616feabb27
LevKorolkov <lev.korolkov4@gmail.com>
2023-03-18 16:58:57 +0300

    Update Randoms.java

--- src/levkorolkov/app/IteratorHomework/Randoms.java
+++ src/levkorolkov/app/IteratorHomework/Randoms.java
@@ -1,31 +1,33 @@
 package levkorolkov.app.IteratorHomework;
 
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
-    PrimitiveIterator.OfInt random;
+    Random random;
     private final int min, max;
 
     public Randoms(int min, int max) {
-        random = new Random().ints(min, (max + 1)).iterator();
+        this.random = new Random();
         this.min = min;
         this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<>() {
-            @Override
-            public boolean hasNext() {
-                return true;
-            }
+        return new RandomIntIterator();
+    }
 
-            @Override
-            public Integer next() {
-                return random.nextInt();
-            }
-        };
+    private class RandomIntIterator implements Iterator<Integer> {
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            return random.nextInt(min, (max + 1));
+        }
     }
 }
